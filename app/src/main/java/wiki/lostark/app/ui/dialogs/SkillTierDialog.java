912fa754c776f433e168014d4a042744e7059eb4
gomsang <gyeongrok.kim@gomsang.com>
2018-12-06 01:36:25 +0900

    modify version to 1.3

--- app/src/main/java/wiki/lostark/app/ui/dialogs/SkillTierDialog.java
+++ app/src/main/java/wiki/lostark/app/ui/dialogs/SkillTierDialog.java
@@ -36,7 +36,6 @@ public class SkillTierDialog extends Dialog {
     private DialogSkillTierBinding binding;
     private CharacterProfileSkill skills;
     private ArrayList<ArrayList<CharacterProfileSkill.Tripod>> tripods;   // 모든 트리팟 리스트 (tripods 0번째 리스트의 0번째 => 첫번째 티어 첫번째 스킬)
-    private int i1 = 1, i2 = 1, i3 = 1;
 
     public SkillTierDialog(Context context, ArrayList<ArrayList<CharacterProfileSkill.Tripod>> tripods, CharacterProfileSkill skills) {
         super(context);
@@ -55,8 +54,12 @@ public class SkillTierDialog extends Dialog {
         setDialogSize(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         setCanceledOnTouchOutside(true);
 
+        applyEnablerTier(skills.getEnableTier());
+        int tripodTurn;
         for (int ti = 0; ti < tripods.size(); ti++) {
+            tripodTurn = 0;
             for (CharacterProfileSkill.Tripod eachTripod : tripods.get(ti)) {
+                tripodTurn++;
                 ImageView imageView = new ImageView(context);
 
                 int px = Math.round(TypedValue.applyDimension(
@@ -93,54 +96,66 @@ public class SkillTierDialog extends Dialog {
                     binding.tier1Layout.addView(imageView);
                     binding.tier1TextLayout.addView(textView);
 
-                    if (skills.getEnableTier() == -1) {
+                    // 모든 티어가 열리지 않았을때 - 모든 티어 암전
+                    if (skills.getEnableTier() == -1)
                         imageView.setColorFilter(Color.parseColor("#eeeeee"), PorterDuff.Mode.SRC_IN);
-                        binding.imgTier1.setColorFilter(Color.parseColor("#eeeeee"), PorterDuff.Mode.SRC_IN);
-                        binding.imgTier2.setColorFilter(Color.parseColor("#eeeeee"), PorterDuff.Mode.SRC_IN);
-                        binding.imgTier3.setColorFilter(Color.parseColor("#eeeeee"), PorterDuff.Mode.SRC_IN);
-                    }
 
-                    if (i1 == skills.getSelectedTripodTier()[0]) {
+
+                    if (tripodTurn == skills.getSelectedTripodTier()[0])
                         textView.setTextColor(Color.parseColor("#217ed3"));
-                    }
 
-                    i1++;
                 }
 
                 if (ti == 1) {
                     binding.tier2Layout.addView(imageView);
                     binding.tier2TextLayout.addView(textView);
 
-                    if (skills.getEnableTier() == -1 || skills.getEnableTier() == 0) {
+                    // 2티어가 열리지 않았을 경우 - 2 티어, 3 티어 암전
+                    if (skills.getEnableTier() < 1)
                         imageView.setColorFilter(Color.parseColor("#eeeeee"), PorterDuff.Mode.SRC_IN);
-                        binding.imgTier2.setColorFilter(Color.parseColor("#eeeeee"), PorterDuff.Mode.SRC_IN);
-                        binding.imgTier3.setColorFilter(Color.parseColor("#eeeeee"), PorterDuff.Mode.SRC_IN);
-                    }
 
-                    if (i2 == skills.getSelectedTripodTier()[1]) {
+
+                    if (tripodTurn == skills.getSelectedTripodTier()[1])
                         textView.setTextColor(Color.parseColor("#7ed321"));
-                    }
 
-                    i2++;
                 }
 
                 if (ti == 2) {
                     binding.tier3Layout.addView(imageView);
                     binding.tier3TextLayout.addView(textView);
 
-                    if (skills.getEnableTier() == -1 || skills.getEnableTier() == 0 || skills.getEnableTier() == 1) {
+                    // 3티어가 열리지 않았을 경우 - 3 티어 티어 암전
+                    if (skills.getEnableTier() < 2)
                         imageView.setColorFilter(Color.parseColor("#eeeeee"), PorterDuff.Mode.SRC_IN);
-                        binding.imgTier3.setColorFilter(Color.parseColor("#eeeeee"), PorterDuff.Mode.SRC_IN);
-                    }
 
-                    if (i3 == skills.getSelectedTripodTier()[2]) {
+
+                    if (tripodTurn == skills.getSelectedTripodTier()[2])
                         textView.setTextColor(Color.parseColor("#d3a821"));
-                    }
 
-                    i3++;
                 }
             }
         }
+    }
+
+    private void applyEnablerTier(int enablerTier) {
+        // 모든 티어가 열리지 않았을때 - 모든 티어 암전
+        if (enablerTier == -1) {
+            binding.imgTier1.setColorFilter(Color.parseColor("#eeeeee"), PorterDuff.Mode.SRC_IN);
+            binding.imgTier2.setColorFilter(Color.parseColor("#eeeeee"), PorterDuff.Mode.SRC_IN);
+            binding.imgTier3.setColorFilter(Color.parseColor("#eeeeee"), PorterDuff.Mode.SRC_IN);
+        }
+
+        // 2티어가 열리지 않았을 경우 - 2 티어, 3 티어 암전
+        if (enablerTier < 1) {
+            binding.imgTier2.setColorFilter(Color.parseColor("#eeeeee"), PorterDuff.Mode.SRC_IN);
+            binding.imgTier3.setColorFilter(Color.parseColor("#eeeeee"), PorterDuff.Mode.SRC_IN);
+        }
+
+        // 3티어가 열리지 않았을 경우 - 3 티어 티어 암전
+        if (enablerTier < 2) {
+            binding.imgTier3.setColorFilter(Color.parseColor("#eeeeee"), PorterDuff.Mode.SRC_IN);
+        }
+
     }
 
     private void setDialogSize(int width, int height) {
